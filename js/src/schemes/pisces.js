592b3ad1519946d2c095df1005e9a34ce6728bf4
胡孝林 <bjhuxiaolin@corp.netease.com>
2016-07-19 09:45:23 +0800

    Site updated: 2016-07-19 09:45:21

--- js/src/schemes/pisces.js
+++ js/src/schemes/pisces.js
@@ -1,6 +1,1 @@
-$(document).ready(function() {
-  var e = $(".header-inner").height() + 10;
-  $("#sidebar").css({
-    "margin-top": e
-  }).show()
-});
+$(document).ready(function(){var e=$(".header-inner").height()+10;$("#sidebar").css({"margin-top":e}).show()});
